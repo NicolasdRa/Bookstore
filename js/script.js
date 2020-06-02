86109dbc9e79de0f3efe6be178d3066c03f26831
Nicolas <nicolasdirago@gmail.com>
2020-06-02 18:29:00 +0200

    Change to New York Times' API, styling adjustments

--- js/script.js
+++ js/script.js
@@ -1,78 +1,73 @@
-// Main variables 
-var title = document.title
-var url = "https://api.myjson.com/bins/zyv02";
-var books = []
+// Main variables
+const title = document.title
+const clientKey = 'tAfZnW8m0Gj3HySOgAGoubHl1QU1omoR'
 
-const search = document.getElementById("search");
+const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${clientKey} `
+let books = []
+
+const search = document.getElementById('search')
 search.addEventListener('keyup', functionController)
-
 
 // Data Request
 fetch(url, {
-        method: "GET"
-    })
+  method: 'GET'
+})
+  .then(function (response) {
+    // console.log(response)
+    return response.json()
+  })
 
-    .then(function (response) {
-        return response.json();
-    })
+  .then(function (data) {
+    console.log(data)
+    books = data.results.books
+    functionController()
+    // console.log(data)
+  })
 
-    .then(function (data) {
-
-        books = data.books
-        functionController()
-    })
-
-    .catch(function (error) {
-        console.log(error)
-    });
-
+  .catch(function (error) {
+    console.log(error)
+  })
 
 // Controller Function
-function functionController() {
-
-    let results = searchBarFilter()
-    console.log(results)
-    createBooks(results)
+function functionController () {
+  const results = searchBarFilter()
+  //   console.log(results)
+  createBooks(results)
 }
-
 
 // Search Bar Filter
-function searchBarFilter() {
-
-    const searchBar = document.getElementById("search").value
-    const text = searchBar.toLowerCase();
-    const results = books.filter(book => {
-        const title = book.title
-        return title.toLowerCase().includes(text)
-    })
-    return results
+function searchBarFilter () {
+  const searchBar = document.getElementById('search').value
+  const text = searchBar.toLowerCase()
+  const results = books.filter(book => {
+    const title = book.title
+    return title.toLowerCase().includes(text)
+  })
+  return results
 }
 
-
 // Creates Main Tables content
-function createBooks(results) {
-    const bookshelf = document.querySelector(".bookshelf")
-    bookshelf.innerHTML = ""
-    results.forEach((book) => {
-
-        var markup =
-            `<div class="flip-card">
+function createBooks (results) {
+  const bookshelf = document.querySelector('.bookshelf')
+  bookshelf.innerHTML = ''
+  results.forEach(book => {
+    const markup = `<div class="flip-card">
         <div class="flip-card-inner">
             <div class="flip-card-front">
-                <img class="flip-card-front__book-cover" src=${book.cover} alt="book cover" style="width:300px;height:300px;">
+                <img class="flip-card-front__book-cover" src=${book.book_image} alt="book cover" style="width:300px;height:300px;">
             </div>
             
             <div class="flip-card-back">
                 <div class="flip-card-back__content">
                     <h3 class="flip-card-back__content-title">${book.title}</h1>
-                    <h4 class="flip-card-back__content-language">Language: ${book.language}</p>
+                    <h4 class="flip-card-back__content-language">Current NYT's Rank: ${book.rank}</p>
                     <p class="flip-card-back__content-description">${book.description}</p>
                 </div>
                 <div class="flip-card-back__button">
-                    <a data-fancybox="gallery" href=${book.detail} class="btn btn--green">More details &roarr;</a>
+                    <a data-fancybox="gallery" href=${book.amazon_product_url} class="btn btn--green">Find on Amazon&roarr;</a>
                 </div>
             </div>`
 
-        bookshelf.innerHTML += markup // el signo += agrega elemento al existente en vez de sobreecribirlo.. ese era el problema, habia utilizado solo el =...//
-    })
+    bookshelf.innerHTML += markup // el signo += agrega elemento al existente en vez de sobreecribirlo.. ese era el problema, habia utilizado solo el =...//
+  })
 }
